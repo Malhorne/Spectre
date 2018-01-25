6ad2172e0547791c76ec22b1c655788f9f1e3437
Matthieu Charly-Desroches <matthieu.charly-desroches@ensimag.fr>
2018-01-25 17:51:36 +0100

    Fix length

--- spectre.c
+++ spectre.c
@@ -17,7 +17,7 @@ uint8_t unused1[64];
 uint8_t array1[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
-char *secret = "The password is Skywalker, I repeat Skywalker";
+char *secret = "The password is Skywalker!";
 uint8_t temp = 0;
 /*  Used so compiler won’t optimize out victim_function() */
 void victim_function(size_t x) {
